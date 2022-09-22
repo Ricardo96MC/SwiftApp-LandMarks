141ebd0d4c7943dd24637b9135ac46cb57e35c25
Ricardo-MAC <ismaelcod4@gmail.com>
2022-09-22 11:59:54 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ricardo Gonzalez on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
